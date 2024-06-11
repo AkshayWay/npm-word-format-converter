02c2e51b45148105a44eff248989c447d3a73929
AkshayWay <38669737+AkshayWay@users.noreply.github.com>
2024-06-11 20:10:04 +0530

    Kebab, train and title case added

--- test/script.js
+++ test/script.js
@@ -1,10 +1,11 @@
-// import { camalize } from "case-format-converter";
-
-// camalize("manish wayangankar").then((res) => console.log(res));
-// const wcf = require("case-format-converter");
-
-// console.log(wcf("camel", "manish way123"));
 const wcf = require("case-format-converter");
 
 console.log(wcf.camelCase("test case format converter library. test case 1"));
 console.log(wcf.snakeCase("test case format converter library. test case 2"));
+console.log(wcf.kebabCase("test case format converter library. test case 3"));
+console.log(
+  wcf.trainCase("test case format converter library. , _ test case 4")
+);
+console.log(
+  wcf.titleCase("test case format converter library. , _ test case 5")
+);
