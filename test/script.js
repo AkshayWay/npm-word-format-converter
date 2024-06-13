7b9f025f43eefc2dfadd12460d511fbb5e51f7df
AkshayWay <38669737+AkshayWay@users.noreply.github.com>
2024-06-14 00:21:30 +0530

    New cases added

--- test/script.js
+++ test/script.js
@@ -1,11 +1,15 @@
 const wcf = require("case-format-converter");
 
-console.log(wcf.camelCase("test case format converter library. test case 1"));
-console.log(wcf.snakeCase("test case format converter library. test case 2"));
-console.log(wcf.kebabCase("test case format converter library. test case 3"));
+console.log(wcf.camelCase("word case format to change string into camelcase"));
+console.log(wcf.snakeCase("word case format to change string into snakecase"));
+console.log(wcf.kebabCase("word case format to change string into kebabcase"));
+console.log(wcf.trainCase("word case format to change string into traincase"));
+console.log(wcf.titleCase("word case format to change string into titlecase"));
 console.log(
-  wcf.trainCase("test case format converter library. , _ test case 4")
+  wcf.pascalCase("word case format to change string into pascalcase")
 );
 console.log(
-  wcf.titleCase("test case format converter library. , _ test case 5")
+  wcf.screamingSnakeCase(
+    "word case format to change string into screming snakecase"
+  )
 );
